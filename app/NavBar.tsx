8f13165da96137adeea1cff1787def232d895af7
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-07-31 16:10:08 +0530

    added issues page and active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { ImBug } from "react-icons/im";
 
@@ -7,6 +9,7 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const path = usePathname();
   return (
     <nav className="flex items-center space-x-5 h-14 px-5 border-b mb-3">
       <Link href={"/"}>
@@ -17,7 +20,9 @@ const NavBar = () => {
           <li key={link.label}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={`${
+                link.href === path ? "text-zinc-900" : "text-zinc-500"
+              }  hover:text-zinc-800 transition-colors`}
             >
               {link.label}
             </Link>
