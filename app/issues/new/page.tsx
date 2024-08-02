382d9b02d052506b3280e6b08aa79376366221dd
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-08-02 13:25:35 +0530

    added useForm and axios

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,47 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import Link from "next/link";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const route = useRouter();
   return (
-    <div className="max-w-lg space-y-4">
+    <form
+      className="max-w-lg space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("http://localhost:3000/api/issues", data);
+        route.push("/");
+      })}
+    >
       <h1>Add New Issue</h1>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <TextArea placeholder="description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <div className="space-x-2">
         <Button>Add New Issue</Button>
         <Button color="gray" variant="outline">
           <Link href={"/"}>Back to Dashboard</Link>
         </Button>
       </div>
-    </div>
+    </form>
   );
 };
 
