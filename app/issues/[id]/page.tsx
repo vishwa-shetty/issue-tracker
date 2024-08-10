92a63a88f913d69dec9b7e94a87bf590ae0880ba
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-08-10 15:07:48 +0530

    added styling to detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,7 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -6,15 +9,22 @@ interface Props {
 }
 
 const IssuesDetailsPage = async ({ params }: Props) => {
+  if (typeof parseInt(params?.id) !== "number") notFound();
+
   const issues = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
+
+  if (!issues) notFound();
+
   return (
     <div>
-      <p>{issues?.title}</p>
-      <p>{issues?.status}</p>
-      <p>{issues?.description}</p>
-      <p>{issues?.createdAt.toDateString()}</p>
+      <Heading as="h1">{issues?.title}</Heading>
+      <Flex gap="3" my="4">
+        <IssueStatusBadge status={issues?.status} />
+        <Text>{issues?.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>{issues?.description}</Card>
     </div>
   );
 };
